8d54e3d59f467b0fb1ccce606a1f0e51ddb7e9b9
jinyoung <jyson19@gmail.com>
2021-04-03 23:16:28 +0900

    UPDATE NumberRange

--- src/etc_basic/NumberRange.java
+++ src/etc_basic/NumberRange.java
@@ -1,18 +1,20 @@
 package etc_basic;
 
+import java.util.stream.IntStream;
+
 public class NumberRange {
-//	static int cnt = 0;
 	public static int solution(int mod1, int mod2, int max_range) {
-		int cnt = 0;
-//		IntStream.range(1, max_range + 1).forEach(i -> {
-		for(int i = 1; i <= max_range; i++) {
-			// mod1로 나눈 나머지가 0이고,
-			// mod2로 나눈 나머지가 있을때
-			if(i % mod1 == 0 && i % mod2 != 0) cnt++;
-		};
-		return cnt;
+		// for문
+//		int cnt = 0;
+//		for(int i = 1; i <= max_range; i++) {
+//			// mod1로 나눈 나머지가 0이고,
+//			// mod2로 나눈 나머지가 있을때
+//			if(i % mod1 == 0 && i % mod2 != 0) cnt++;
+//		};
+//		return cnt;
 		
-//		return (int) IntStream.range(1, max_range + 1).filter(i -> i % mod1 == 0 && i % mod2 != 0).count();
+		// stream
+		return (int) IntStream.range(1, max_range + 1).filter(i -> i % mod1 == 0 && i % mod2 != 0).count();
 	}
 	public static void main(String[] args) {
 		int mod1 = 4;
--- src/etc_basic/Tsn.java
+++ src/etc_basic/Tsn.java
@@ -3,14 +3,18 @@ package etc_basic;
 import java.util.stream.IntStream;
 
 public class Tsn {
-	static int cnt = 0;
 	public static int solution(int num) {
-		IntStream.range(1, num + 1).forEach(i -> {
-			String a = i + "";
-			if(a.contains("3") || a.contains("6") || a.contains("9"))
-				cnt++;
-			});
-		return cnt;
+		// for문
+//		int cnt = 0;
+//		for(int i = 1; i <= num; i++) {
+//			String a = i + "";
+//			if(a.contains("3") || a.contains("6") || a.contains("9"))
+//			cnt++;
+//		};
+//		return cnt;
+		
+		// stream
+		return (int) IntStream.range(1, num + 1).mapToObj(i -> i + "").filter(i -> i.contains("3") || i.contains("6") || i.contains("9")).count();
 	}
 	public static void main(String[] args) {
 //		int num = 10; // 3
